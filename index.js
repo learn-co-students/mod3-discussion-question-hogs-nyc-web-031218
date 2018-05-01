a333def0e09eafaf6ad7c8c50941a83a60adb89c
Eric Kim <hyeokjungkim@gmail.com>
2018-05-01 10:00:58 -0400

    Done.

--- index.js
+++ index.js
@@ -27,24 +27,37 @@ function transitionPage(el, groupOut, groupIn) {
 
 function delayedFadeOut(div, range) {
   // Your solution here
-  fadeOut(div)
+  // fadeOut(div)
+  setTimeout(() => {
+    fadeOut(div)
+  }, range)
 }
 
 function delayedFadeIn(div, range) {
   // Your solution here
-  fadeIn(div)
+  setTimeout(() => {
+    fadeIn(div)
+  }, range)
 }
 
 function fadeAllOut(el, group) {
   // Your solution here
+  timeoutValue = SHORTRANGE
   group.forEach(div => {
-    delayedFadeOut(div)
+    if(div === el){
+      delayedFadeOut(div, LONGRANGE)
+    }else{
+    delayedFadeOut(div, timeoutValue)
+    timeoutValue += MINDELAY
+  }
   })
 }
 
 function fadeAllIn(group) {
   // Your solution here
+  timeoutValue = SHORTRANGE
   group.forEach(div => {
-    delayedFadeIn(div)
+    delayedFadeIn(div, timeoutValue)
+    timeoutValue += MINDELAY
   })
 }
