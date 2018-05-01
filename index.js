21254182346cf6d24c7decf1c54ea936dc36cf44
victoria-huang <vctrah@gmail.com>
2018-05-01 09:46:50 -0400

    delay fade in fade out

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ function transitionPage(el, groupOut, groupIn) {
 
 function delayedFadeOut(div, range) {
   // Your solution here
-  fadeOut(div)
+  setTimeout(() => {fadeOut(div)}, range);
 }
 
 function delayedFadeIn(div, range) {
   // Your solution here
-  fadeIn(div)
+  setTimeout(() => {fadeIn(div)}, range);
 }
 
 function fadeAllOut(el, group) {
   // Your solution here
   group.forEach(div => {
-    delayedFadeOut(div)
+    (div === el) ? delayedFadeOut(div, 300) : delayedFadeOut(div, Math.random() * 200)
   })
 }
 
 function fadeAllIn(group) {
   // Your solution here
   group.forEach(div => {
-    delayedFadeIn(div)
+    delayedFadeIn(div, Math.random() * 500)
   })
 }
